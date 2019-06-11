a230d06526c743c38bdc22800cfb32a4374031e5
judesama <judeagagad@gmail.com>
2019-06-12 01:43:46 +0800

    attr

--- scripts/app.js
+++ scripts/app.js
@@ -10,14 +10,25 @@ $(document).ready(function(){
             $("#navigation").css("background", "rgba(0, 0, 0, 0.2)");
             $("#navigation").removeClass('box-shadow')
             $(".nav-link").css("color" , "#333");
+            $(".nav-link").css("transition" , "1s");
+            
         } 
         
         else if(st < 500){
             $("#navigation").css("background", "#333");
             $(".nav-link").css("color" , "#FFF");
             
-            
+            $(".nav-link").css("transition" , "1s");
             $("#navigation").addClass('box-shadow')
+
+            $('.nav-link').hover(function(){
+                $(this).css("color", "#111");
+                $(this).css("font-size", "17px");
+                
+            } , function(){
+                $(this).css("color", "#333");
+                $(this).css("font-size", "16px");
+            })
         }
         lastScrollTop = st;
      });
@@ -26,9 +37,10 @@ $(document).ready(function(){
     console.log("asd");
     getURL('https://api.opendota.com/api/heroStats')
         .then(function(data){
-            console.log(data);
             data.reverse().forEach(hero => {
-              
+                
+                let attr = hero.primary_attr;
+                
                 let heroDisplay = $(`
                 
                
@@ -36,7 +48,7 @@ $(document).ready(function(){
                     <div class="card hero-card" style="width: 18rem;">
                         <img class="card-img-top" src="https://api.opendota.com${hero.img}" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">${hero.localized_name}</h5>
+                            <h5 class="card-title">${hero.localized_name} <small>(${attr})</small></h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a class="btn btn-secondary" href="#" role="button" data-toggle="modal" data-target="#${hero.name}-card">View details &raquo;</a>
                         </div>
